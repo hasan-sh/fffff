01392fcc4d75148c21005ca956a889336cc59919
Hasan Sh <hasansh343@gmail.com>
2023-04-10 16:25:59 +0200

    unneeded files and cleaned columns (WIP)

--- exploration/utils.py
+++ exploration/utils.py
@@ -1,4 +1,5 @@
 import string
+from collections import Counter
 
 def replace_punctuation_with_space(text):
     # Create a translation table with punctuation replaced by spaces
@@ -6,3 +7,10 @@ def replace_punctuation_with_space(text):
 
     # Use the translation table to replace punctuation with spaces
     return text.translate(translator)
+
+
+def get_ocm_counts(df, id_to_category=None, limit=626):
+    ocms_list = df['ocms'].str.split(expand=True).values.ravel()
+    ocms_list = [id_to_category[int(i)] if id_to_category else int(i) for i in ocms_list if i and int(i) <= limit]
+    ocms_counts = Counter(ocms_list)
+    return ocms_counts
