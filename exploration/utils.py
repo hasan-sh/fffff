44b2114a423efa1c901811a0ec00900bbc69be28
Hasan Sh <hasansh343@gmail.com>
2023-04-11 17:01:50 +0200

    exploration + WIP preprocessing

--- exploration/utils.py
+++ exploration/utils.py
@@ -14,3 +14,15 @@ def get_ocm_counts(df, id_to_category=None, limit=626):
     ocms_list = [id_to_category[int(i)] if id_to_category else int(i) for i in ocms_list if i and int(i) <= limit]
     ocms_counts = Counter(ocms_list)
     return ocms_counts
+
+
+def make_id_category(ocms, label_to_id):
+    id_to_category = {000: 000}
+    for item in ocms:
+        category, values = list(item.items())[0]
+        cat_id = label_to_id[category]
+
+        id_to_category[cat_id] = cat_id
+        for x in values:
+            id_to_category[int(x[0])] = cat_id
+    return id_to_category
